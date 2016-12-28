01b38d48f1201d0b18e9ce879c2278e6b8da6376
Dim <dmotorny@gmail.com>
2016-12-28 15:34:29 +0200

    new rep

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
-(function() {
+const app = angular.module('myApp', []);
 
+(function() {
+    
     // Initialize Firebase
     var config = {
       apiKey: "AIzaSyAhiefIsbzCYeWDi9b5LV-R80-mNRzfKZw",
@@ -7,7 +9,7 @@
       databaseURL: "https://mailbox-87bdf.firebaseio.com",
       storageBucket: "mailbox-87bdf.appspot.com",
       messagingSenderId: "765645523270"
-    };   
+    };  
     
     firebase.initializeApp(config); 
 
@@ -21,12 +23,41 @@
         const email = fbEmail.value;
         const pass = fbPassword.value;
         const auth = firebase.auth();
-
         // Sign in
-
         const promise = auth.signInWithEmailAndPassword(email, pass);
         promise.catch(e => console.log(e.message));
+    });
 
+    btSignup.addEventListener('click', e => {
+        // TODO: CHECK 4 REAL EMAILS
+        const email = fbEmail.value;
+        const pass = fbPassword.value;
+        const auth = firebase.auth();
+        // Sign in
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+        promise
+            // .then(user => console.log(user))
+            .catch(e => console.log(e.message));
+    });
+
+    btSignout.addEventListener('click', e => {
+        fbEmail.value = '';
+        fbPassword.value = '';
+        firebase.auth().signOut();
+
+    });
+
+    // Add a realtime listener
+
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            console.log('logged in');
+            // console.log(firebaseUser);
+            btSignout.classList.remove('hide');            
+        } else {
+            console.log('logged out');
+            btSignout.classList.add('hide');
+        }
     });
 
 }());
